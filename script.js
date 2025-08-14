a7722e006b88f97f25e74e4ae2bd55effa4202a9
AW492 <studentavaniqueweir@gmail.com>
2025-08-14 18:44:19 -0500

    script with api handling and button action for display

--- script.js
+++ script.js
@@ -15,3 +15,50 @@ var display_data_button = document.getElementById('display-data-button');
 var return_button = document.getElementById('return-patient-input');
 
 
+//handle data submitted by form and does the post request 
+
+form.addEventListener('submit', async(e) => {
+
+    e.preventDefault(); //preventing default reload of webpage 
+
+    var patientName = document.getElementById('name').value;
+    var patientAge = parseInt(document.getElementById('age').value);
+
+    try{
+        const response = await fetch(`${API_url}/submit`, {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },body: JSON.stringify({patientName,patientAge})// these are inputs collected from the form so add accordingly 
+        });
+        const responseBody = await response.json();
+        patientDataResponse.textContent = JSON.stringify(responseBody,null,2); //thes have to do with how many inputs we are taking in so increase count accordingly 
+        form.reset();
+
+    } catch (err) {
+        patientDataResponse.textContent = 'Error found: ' + err;
+    }
+
+});
+
+
+//handle the get request to see that stored patient data 
+
+display_data_button.addEventListener('click', async ()=> {
+    try {
+        const response = await fetch(`${API_url}/data`);
+
+        const responseBody = await response.json();
+        patientDataStored.textContent = JSON.stringify(responseBody, null, 2);
+        patientDataInput.classList.remove('active');
+        patientDataView.classList.add('active');
+    } catch (err) {
+        patientDataStored.textContent = 'Error found:' + err;
+    }
+});
+
+// taking us back to the input patient data section 
+return_button.addEventListener('click',() =>{
+    patientDataView.classList.remove('active');
+    patientDataInput.classList.add('active');
+});
