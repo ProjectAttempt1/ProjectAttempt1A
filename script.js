36a80c0bcd4c4e9feb529c0e6f666faabd5f0705
AW492 <studentavaniqueweir@gmail.com>
2025-08-14 17:57:05 -0500

    additions to index script and style

---
+++ script.js
@@ -0,0 +1,17 @@
+const API_url = ''; //Render URL 
+
+var patientDataInput = document.getElementById('patient-data-input');
+
+var patientDataView = document.getElementById('patient-data-view');
+
+var patientDataForm = document.getElementById('patient-data-form');
+
+var patientDataResponse = document.getElementById('patient-data-response');
+
+var patientDataStored = document.getElementById('stored-patient-data');
+
+var display_data_button = document.getElementById('display-data-button');
+
+var return_button = document.getElementById('return-patient-input');
+
+
